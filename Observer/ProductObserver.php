dd5469df7f54c5412e89dd6b530b62a219d4323d
Christiaan de Ridder <ik@christiaanderidder.nl>
2019-02-01 16:39:23 +0100

    Add a custom timestamp attribute to allow for incremental updates

---
+++ Observer/ProductObserver.php
@@ -0,0 +1,22 @@
+<?php namespace ChannelEngine\Magento2\Observer;
+
+use Magento\Framework\Event\ObserverInterface;
+
+class ProductObserver implements ObserverInterface
+{
+	public function __construct()
+	{
+		
+	}
+
+	public function execute(\Magento\Framework\Event\Observer $observer)
+	{
+		$product = $observer->getProduct();
+		$date = date('Y-m-d H:i:s');
+		$attr = 'ce_updated_at';
+
+		// Set both: https://magento.stackexchange.com/a/229280
+        $product->setData($attr, $date);
+		$product->setCustomAttribute($attr, $date);
+	}
+}
---
+++ Setup/UpgradeData.php
@@ -0,0 +1,125 @@
+<?php namespace ChannelEngine\Magento2\Setup;
+
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\UpgradeDataInterface;
+use Magento\Framework\DB\Ddl\Table;
+use Magento\Integration\Model\ConfigBasedIntegrationManager;
+use Magento\Sales\Setup\SalesSetupFactory;
+use Magento\Sales\Model\Order;
+use Magento\Catalog\Model\Product;
+use Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface;
+use Magento\Eav\Setup\EavSetupFactory;
+
+class UpgradeData implements UpgradeDataInterface
+{
+    const QUOTE_ENTITY = 'quote';
+
+    /**
+    * @var ConfigBasedIntegrationManager
+    */
+    private $integrationManager;
+
+    /**
+     * @var SalesSetupFactory
+     */
+    protected $salesSetupFactory;
+
+    /**
+     * @var EavSetupFactory
+     */
+    protected $eavSetupFactory;
+
+    /**
+    * @param ConfigBasedIntegrationManager $integrationManager
+    */
+    public function __construct(ConfigBasedIntegrationManager $integrationManager, SalesSetupFactory $salesSetupFactory, EavSetupFactory $eavSetupFactory)
+    {
+        $this->integrationManager = $integrationManager;
+        $this->salesSetupFactory = $salesSetupFactory;
+        $this->eavSetupFactory = $eavSetupFactory;
+
+        $this->orderAttributes = [
+            'ce_id' => [
+                'type' => Table::TYPE_INTEGER,
+                'visible' => true,
+                'required' => false,
+                'label' => 'CE Order ID'
+            ],
+            'ce_channel_order_no' => [
+                'type' => Table::TYPE_TEXT,
+                'visible' => true,
+                'required' => false,
+                'label' => 'CE Channel Order No'
+            ],
+            'ce_channel_name' => [
+                'type' => Table::TYPE_TEXT,
+                'visible' => true,
+                'required' => false,
+                'label' => 'CE Channel Name'
+            ]
+        ];
+        $this->orderLineAttributes = [
+            'ce_id' => [
+                'type' => Table::TYPE_INTEGER,
+                'visible' => true,
+                'required' => false,
+                'label' => 'CE Order Line ID'
+            ]
+        ];
+        $this->productAttributes = [
+            'ce_updated_at' => [
+                'type' => Table::TYPE_DATETIME,
+                'visible' => false,
+                'input' => 'date',
+                'required' => false,
+                'user_defined' => false,
+                'default' => '2019-01-01 00:00:00',
+                'global' => ScopedAttributeInterface::SCOPE_GLOBAL,
+                'label' => 'CE last product update'
+            ]
+        ];
+    }
+
+    /**
+    * {@inheritdoc}
+    */
+    public function upgrade(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $setup->startSetup();
+
+        $conn = $setup->getConnection();
+        $orderGridTable = $setup->getTable('sales_order_grid');
+
+        // Install attributes
+        $salesSetup = $this->salesSetupFactory->create(['resourceName' => 'sales_setup', 'setup' => $setup]);
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
+
+        foreach ($this->orderAttributes as $attr => $config)
+        {
+            $conn->addColumn($orderGridTable, $attr, [
+                'type' => $config['type'],
+                'length' => 255,
+                'nullable' => true,
+                'comment' => $config['label']
+            ]);
+
+            $salesSetup->addAttribute(Order::ENTITY, $attr, $config);
+        }
+
+        foreach ($this->orderLineAttributes as $attr => $config)
+        {
+            $salesSetup->addAttribute('order_item', $attr, $config);
+        }
+
+        foreach ($this->productAttributes as $attr => $config)
+        {
+            $eavSetup->addAttribute(Product::ENTITY, $attr, $config);
+        }
+
+        // Install integrations
+        $this->integrationManager->processIntegrationConfig(['ChannelEngine']);
+
+        $setup->endSetup();
+    }
+}
