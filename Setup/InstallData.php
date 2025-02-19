a0f5e63e4b7df2313f913f6ba89309d7c76476f2
Istvan Sinkler-Nemeth <4548581+isinkler@users.noreply.github.com>
2025-02-19 17:58:59 +0200

    Use installdata

---
+++ Setup/InstallData.php
@@ -0,0 +1,52 @@
+<?php namespace ChannelEngine\Magento2\Setup;
+
+use Magento\Catalog\Model\Product;
+use Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface;
+use Magento\Framework\Setup\InstallDataInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+
+class InstallData implements InstallDataInterface
+{
+    protected $eavSetupFactory;
+
+    public function __construct(
+        \Magento\Eav\Setup\EavSetupFactory $eavSetupFactory
+    )
+    {
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+
+    /**
+     * @param ModuleDataSetupInterface $setup
+     * @param ModuleContextInterface $context
+     * {@inheritDoc}
+     *
+     * @SuppressWarnings(PHPMD.CyclomaticComplexity)
+     * @suppressWarnings(PHPMD.ExessiveMethodLength)
+     * @SuppressWarnings(PHPMD.NPathComplexity)
+     */
+    public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $eavSetup = $this->eavSetupFactory->create();
+        $eavSetup->addAttribute(
+            Product::ENTITY,
+            'ce_updated_at',
+            [
+                'group'         => 'General',
+                'type'          => 'datetime',
+                'label'         => 'CE last product update',
+                'input'         => 'date',                
+                'required'      => false,
+                'sort_order'    => 50,
+                'global'        => ScopedAttributeInterface::SCOPE_GLOBAL,
+                'is_used_in_grid'               => false,
+                'is_visible_in_grid'            => false,
+                'is_filterable_in_grid'         => false,
+                'visible'                       => false,
+                'is_html_allowed_on_frontend'   => false,
+                'visible_on_front'              => false,
+            ]
+        );
+    }
+}
