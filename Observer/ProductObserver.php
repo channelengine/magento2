80991ad36db14fab9f3936aceae35ec250cb0b4f
isinkler <4548581+isinkler@users.noreply.github.com>
2025-02-21 15:54:32 +0200

    Use plugin approach

--- Observer/ProductObserver.php
+++ Observer/ProductObserver.php
@@ -19,26 +19,26 @@ class ProductObserver implements ObserverInterface
 
     public function execute(\Magento\Framework\Event\Observer $observer)
     {
-        $product = $observer->getEvent()->getProduct();
-        $productId = $product->getId();
-
-        try {
-            // Load the product again to avoid conflicts
-            $loadedProduct = $this->productRepository->getById($productId);
-
-            $date = date('Y-m-d H:i:s');
-            $attr = 'ce_updated_at';
-
-            // Set the attribute value (Replace 'your_attribute_code' with the actual attribute code)
-            $loadedProduct->setCustomAttribute($attr, $date);
-            $loadedProduct->setData($attr, $date); // Alternative way
-
-            // Save the updated product
-            $this->productRepository->save($loadedProduct);
-
-            $this->logger->info('Updated product attribute for Product ID: ' . $productId);
-        } catch (\Exception $e) {
-            $this->logger->error('Error updating product attribute: ' . $e->getMessage());
-        }
+//        $product = $observer->getEvent()->getProduct();
+//        $productId = $product->getId();
+//
+//        try {
+//            // Load the product again to avoid conflicts
+//            $loadedProduct = $this->productRepository->getById($productId);
+//
+//            $date = date('Y-m-d H:i:s');
+//            $attr = 'ce_updated_at';
+//
+//            // Set the attribute value (Replace 'your_attribute_code' with the actual attribute code)
+//            $loadedProduct->setCustomAttribute($attr, $date);
+//            $loadedProduct->setData($attr, $date); // Alternative way
+//
+//            // Save the updated product
+//            $this->productRepository->save($loadedProduct);
+//
+//            $this->logger->info('Updated product attribute for Product ID: ' . $productId);
+//        } catch (\Exception $e) {
+//            $this->logger->error('Error updating product attribute: ' . $e->getMessage());
+//        }
     }
 }
---
+++ Plugin/UpdateCeUpdatedAtAttribute.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace ChannelEngine\Magento2\Plugin;
+
+use Magento\Catalog\Api\Data\ProductInterface;
+use Magento\Catalog\Api\ProductRepositoryInterface;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Stdlib\DateTime\DateTime;
+use Psr\Log\LoggerInterface;
+
+class UpdateCeUpdatedAtAttribute
+{
+
+    /**
+     * @var DateTime
+     */
+    protected $dateTime;
+
+    /**
+     * @var LoggerInterface
+     */
+    protected $logger;
+
+    /**
+     * Constructor
+     *
+     * @param DateTime $dateTime
+     * @param LoggerInterface $logger
+     */
+    public function __construct(DateTime $dateTime, LoggerInterface $logger)
+    {
+        $this->dateTime = $dateTime;
+        $this->logger = $logger;
+    }
+
+    /**
+     * Before save plugin for ProductRepositoryInterface save method
+     *
+     * @param ProductRepositoryInterface $subject
+     * @param ProductInterface $product
+     * @return ProductInterface|ProductInterface[]
+     * @throws LocalizedException
+     */
+    public function beforeSave(ProductRepositoryInterface $subject, ProductInterface $product) {
+
+        $currentDate = $this->dateTime->gmtDate();
+
+        try {
+            $product->setCustomAttribute('ce_updated_at', $currentDate);
+            $product->setData('ce_updated_at', $currentDate);
+            return [$product];
+        } catch (\Exception $e) {
+            $this->logger->error(
+                'Error occurred while updating ce_updated_at attribute for product ID ' . $product->getId(),
+                ['exception' => $e]
+            );
+
+            return [$product];
+        }
+    }
+}
