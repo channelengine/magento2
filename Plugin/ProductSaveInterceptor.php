c42457a295c4ee35c0189e26ce6325b48f73cbc3
isinkler <4548581+isinkler@users.noreply.github.com>
2025-02-24 21:45:42 +0200

    Add throttle

--- Plugin/ProductSaveInterceptor.php
+++ Plugin/ProductSaveInterceptor.php
@@ -3,6 +3,7 @@
 namespace ChannelEngine\Magento2\Plugin;
 
 use Magento\Catalog\Model\Product;
+use Magento\Framework\Stdlib\DateTime\DateTime;
 use Psr\Log\LoggerInterface;
 
 class ProductSaveInterceptor
@@ -12,15 +13,22 @@ class ProductSaveInterceptor
      * @var LoggerInterface
      */
     protected $logger;
+    /**
+     * @var DateTime
+     */
+    private DateTime $dateTime;
 
     /**
      * Constructor
      *
      * @param LoggerInterface $logger
      */
-    public function __construct(LoggerInterface $logger)
+    public function __construct(
+        LoggerInterface $logger,
+        DateTime  $dateTime)
     {
         $this->logger = $logger;
+        $this->dateTime = $dateTime;
     }
 
     public function beforeSave(Product $subject)
@@ -28,9 +36,16 @@ class ProductSaveInterceptor
         // This beforeSave is being called when the Product is saved on the UI manually
 
         $this->logger->debug('Called Product beforeSave...');
-        $date = date('Y-m-d H:i:s');
 
         try {
+            $minutesDiff = $this->getLastUpdatedAtDifference($subject);
+
+            if ($minutesDiff <= 5) {
+                return null;
+            }
+
+            $date = $this->dateTime->gmtDate();
+
             $subject->setData('ce_updated_at', $date);
             $subject->setCustomAttribute('ce_updated_at', $date);
 
@@ -44,4 +59,47 @@ class ProductSaveInterceptor
             return null;
         }
     }
+
+    /**
+     * @param $product
+     * @return float|int
+     * @throws \DateMalformedStringException
+     */
+    public function getLastUpdatedAtDifference($product)
+    {
+        $lastUpdatedAt = $product->getUpdatedAt();
+
+        $currentTime = $this->dateTime->gmtDate();
+
+        $productUpdatedAtTime = new \DateTime($lastUpdatedAt);
+        $currentTimeObj = new \DateTime($currentTime);
+
+        // Calculate the difference in minutes
+        $interval = $productUpdatedAtTime->diff($currentTimeObj);
+        $minutesDiff = $interval->i + ($interval->d * 24 * 60) + ($interval->h * 60);
+
+        return $minutesDiff;
+    }
+
+//    public function afterSave(Product $subject, Product $result)
+//    {
+//        try {
+//            // Example: Log the product ID and update a custom datetime attribute
+//            $productId = $result->getId();
+//            $this->logger->info("Product {$productId} was saved.");
+//
+//            $date = date('Y-m-d H:i:s');
+//
+//            $subject->setData('ce_updated_at', $date);
+//            $subject->setCustomAttribute('ce_updated_at', $date);
+//
+////            // Save the product again (optional, but might trigger another plugin execution)
+////            $result->save();
+//
+//        } catch (\Exception $e) {
+//            $this->logger->error("Error in ProductPlugin afterSave: " . $e->getMessage());
+//        }
+//
+//        return $result;
+//    }
 }
--- Plugin/StockItemSaveInterceptor.php
+++ Plugin/StockItemSaveInterceptor.php
@@ -7,6 +7,7 @@ use Magento\CatalogInventory\Api\Data\StockItemInterface;
 use Magento\Catalog\Model\ProductRepository;
 use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Framework\Exception\CouldNotSaveException;
+use Magento\Framework\Stdlib\DateTime\DateTime;
 use Psr\Log\LoggerInterface;
 
 class StockItemSaveInterceptor
@@ -20,15 +21,23 @@ class StockItemSaveInterceptor
      * @var ProductRepository
      */
     protected $productRepository;
+    /**
+     * @var DateTime
+     */
+    private DateTime $dateTime;
 
     /**
      * @param ProductRepository $productRepository
      * @param LoggerInterface $logger
      */
-    public function __construct(ProductRepository $productRepository, LoggerInterface $logger)
+    public function __construct(
+        ProductRepository $productRepository,
+        LoggerInterface $logger,
+        DateTime $dateTime)
     {
         $this->productRepository = $productRepository;
         $this->logger = $logger;
+        $this->dateTime = $dateTime;
     }
 
     /**
@@ -47,11 +56,19 @@ class StockItemSaveInterceptor
         $this->logger->debug('Called StockItem afterSave...');
 
         $productId = $result->getProductId();
-        $date = date('Y-m-d H:i:s');
+
 
         if ($productId) {
             try {
                 $product = $this->productRepository->getById($productId);
+
+                $minutesDiff = $this->getLastUpdatedAtDifference($product);
+
+                if ($minutesDiff <= 5) {
+                    return $result; // Do nothing
+                }
+
+                $date = $this->dateTime->gmtDate();
 
                 $product->setData('ce_updated_at', $date);
                 $product->setCustomAttribute('ce_updated_at', $date);
@@ -59,12 +76,35 @@ class StockItemSaveInterceptor
                 // Save only the attribute, to prevent cyclic events (when already performing a product save)
                 $product->getResource()->saveAttribute($product, 'ce_updated_at');
             } catch (NoSuchEntityException $e) {
+                // TODO: log instead and return $result to do nothing
                 throw new NoSuchEntityException(__('Product not found: %1', $productId));
             } catch (\Exception $e) {
+                // TODO: just log and do nothing
                 throw new CouldNotSaveException(__('Could not save product: %1', $e->getMessage()));
             }
         }
 
         return $result;
+    }
+
+    /**
+     * @param $product
+     * @return float|int
+     * @throws \DateMalformedStringException
+     */
+    public function getLastUpdatedAtDifference($product)
+    {
+        $lastUpdatedAt = $product->getUpdatedAt();
+
+        $currentTime = $this->dateTime->gmtDate();
+
+        $productUpdatedAtTime = new \DateTime($lastUpdatedAt);
+        $currentTimeObj = new \DateTime($currentTime);
+
+        // Calculate the difference in minutes
+        $interval = $productUpdatedAtTime->diff($currentTimeObj);
+        $minutesDiff = $interval->i + ($interval->d * 24 * 60) + ($interval->h * 60);
+
+        return $minutesDiff;
     }
 }
