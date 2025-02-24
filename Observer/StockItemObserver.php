e8edf8631d0f434fb314fda5d138b2412476a2f4
isinkler <4548581+isinkler@users.noreply.github.com>
2025-02-24 15:21:36 +0200

    Uncomment StockItemObserver

--- Observer/StockItemObserver.php
+++ Observer/StockItemObserver.php
@@ -1,5 +1,6 @@
 <?php namespace ChannelEngine\Magento2\Observer;
 
+use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
 use Magento\Catalog\Model\ProductRepository;
 
@@ -15,23 +16,23 @@ class StockItemObserver implements ObserverInterface
         $this->productRepository = $productRepository;
     }
 
-    public function execute(\Magento\Framework\Event\Observer $observer)
+    public function execute(Observer $observer)
     {
-//        try {
-//            $stockItem = $observer->getItem();
-//            $productId = $stockItem->getProductId();
-//            $product = $this->productRepository->getById($productId);
-//            $date = date('Y-m-d H:i:s');
-//            $attr = 'ce_updated_at';
-//
-//            // Set both: https://magento.stackexchange.com/a/229280
-//            $product->setData($attr, $date);
-//            $product->setCustomAttribute($attr, $date);
-//
-//            // Save only the attribute, to prevent cyclic events (when already performing a product save)
-//            $product->getResource()->saveAttribute($product, $attr);
-//        } catch (\Exception $e) {
-//            // Ignoring edge case error when multiple products are being updated at once (ex. performance test)
-//        }
+        try {
+            $stockItem = $observer->getItem();
+            $productId = $stockItem->getProductId();
+            $product = $this->productRepository->getById($productId);
+            $date = date('Y-m-d H:i:s');
+            $attr = 'ce_updated_at';
+
+            // Set both: https://magento.stackexchange.com/a/229280
+            $product->setData($attr, $date);
+            $product->setCustomAttribute($attr, $date);
+
+            // Save only the attribute, to prevent cyclic events (when already performing a product save)
+            $product->getResource()->saveAttribute($product, $attr);
+        } catch (\Exception $e) {
+            // Ignoring edge case error when multiple products are being updated at once (ex. performance test)
+        }
     }
 }
